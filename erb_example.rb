186b6d111c0072f963a201ba1352180a0be77c57
Luthan Hill <luthanhill3@gmail.com>
2013-05-20 21:51:58 -0500

    homework section 1 complete

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,15 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+name = "Loot"
+template = ERB.new "My name is <%= name %>"
+puts template.result(binding)
+
+cities = ['New York', 'Houston', 'Miami']
+template = ERB.new "I love <%= cities %>"
+puts template.result(binding)
+
+cars = {:Lamborghini => 'Gallardo', :Ferrari => 'F430', :Porsche => 'Panamera'}
+template = ERB.new "<%= cars %>"
+puts template.result(binding)
